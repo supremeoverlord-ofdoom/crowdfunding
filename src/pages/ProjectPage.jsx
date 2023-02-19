0946198ad5f8f83569f2668e4da6d1bad5273704
supremeoverlord-ofdoom <108652766+supremeoverlord-ofdoom@users.noreply.github.com>
2023-02-19 23:27:06 +0800

    formatted date field
    formatted with 'date-fns' javascript library

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; 
 
+// extra libraries 
+import { format } from "date-fns";
+
 function ProjectPage() {
     // State
     const [projectData, setProjectData] = useState({ pledges: [] });
@@ -19,30 +22,36 @@ function ProjectPage() {
         });
     }, []);
 
-return (
-    <div>
-      <h2>{projectData.title}</h2>
-      <h3>Project created on: {projectData.date_created}</h3>
-      <h3>{`Total gnomes pledged: ${projectData.total}`}</h3>
-      <h3>{`Open to new pledges: ${projectData.is_open}`}</h3>
-      <h3>Project Description</h3>
-      <p>{projectData.description}</p>
-      <div>
-        <img src={projectData.image} />
-      </div>
-      <h3>Pledges:</h3>
-      <ul>
-        {projectData.pledges.map((pledgeData, key) => {
-          return (
-            <li>
-              {pledgeData.amount} gnomes donated from {pledgeData.supporter_public} - "{pledgeData.comment}"
-            </li>
-          );
-        })}
-      </ul>
+    const formattedDate = projectData.date_created
+    ? format(new Date(projectData.date_created), "do MMMM y - GGGG G")
+    : "";
+
+    return (
+        <div>
+        <h2>{projectData.title}</h2>
+        <h3>Project created on: {formattedDate}</h3>
+
+        <h3>{`Total gnomes pledged: ${projectData.total}`}</h3>
+        <h3>{`Open to new pledges: ${projectData.is_open}`}</h3>
+        <h3>Project Description</h3>
+        <p>{projectData.description}</p>
+        <div>
+            <img src={projectData.image} />
+        </div>
+        <h3>Pledges:</h3>
+        <ul>
+            {projectData.pledges.map((pledgeData, key) => {
+            return (
+                <li>
+                {pledgeData.amount} gnomes donated from {pledgeData.supporter_public} - "{pledgeData.comment}"
+                </li>
+                );
+            })}
+        </ul>
     </div>
-  );
+);
 }
 
 
 export default ProjectPage;
+
