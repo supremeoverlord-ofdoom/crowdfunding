ed5663c3c9cdbd6607054a2816b2bdfd77d6a062
supremeoverlord-ofdoom <108652766+supremeoverlord-ofdoom@users.noreply.github.com>
2023-03-06 11:44:07 +0800

    media queries nav bar
    made small-nav hamburger, hides when display >600 and main nav hides when display <600

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,7 +9,7 @@ function Nav(props) {
     }
     return (
         <nav>
-            <div id="nav-right">
+            <div class="main-nav">
                 {!loggedIn && <Link to="/login" className="btn">Login</Link>}
                 {!loggedIn && <Link to="/users" className="btn">Create Account</Link>}
                 {loggedIn && <button onClick={handleClick} className="btn">Sign Out</button>}
@@ -21,7 +21,23 @@ function Nav(props) {
                     <Link to="/" >Home</Link>
                 </div>
             </div>
-        </nav>
+      <div class="small-nav" role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+            <Link to="/"><li>Home</li></Link>
+            <Link to="/projects"><li>Explore Projects</li></Link>
+            {!loggedIn && <Link to="/login"><li>Login</li></Link>}
+            {!loggedIn && <Link to="/users"><li>Create Account</li></Link>}
+            {loggedIn && <Link to="/create-project"><li>Create Project</li></Link>}
+            {loggedIn && <button onClick={handleClick} className="btn">Sign Out</button>}
+          </ul>
+        </div>
+      </div>
+    </nav>
     );
 }
 export default Nav;
